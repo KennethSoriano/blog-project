f398ad2257a7d9b8f53e182e231ee6d9b66fbeba
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-09-07 20:10:03 -0700

    AddPostForm redo

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,81 +1,53 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
 
-import { addNewPost } from "./postsSlice";
-import { selectAllUsers } from "../users/usersSlice";
+import { postAdded } from "./postsSlice";
 
 const AddPostForm = () => {
-    const dispatch = useDispatch()
-
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
-    const [userId, setUserId] = useState('')
-    const [addRequestStatus, setAddRequestStatus] = useState('idle')
-
-    const users = useSelector(selectAllUsers)
+    const dispatch = useDispatch()
 
     const onTitleChanged = e => setTitle(e.target.value)
     const onContentChanged = e => setContent(e.target.value)
-    const onAuthorChanged = e => setUserId(e.target.value)
-
-
-    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
 
     const onSavePostClicked = () => {
-        if (canSave) {
-            try {
-                setAddRequestStatus('pending')
-                dispatch(addNewPost({ title, body: content, userId })).unwrap()
-
-                setTitle('')
-                setContent('')
-                setUserId('')
-            } catch (err) {
-                console.error('Failed to save the post', err)
-            } finally {
-                setAddRequestStatus('idle')
-            }
+        if (title && content) {
+            dispatch(
+                postAdded(title, content)
+            )
+            setContent('')
+            setContent('')
         }
-
     }
 
-    const usersOptions = users.map(user => (
-        <option key={user.id} value={user.id}>
-            {user.name}
-        </option>
-    ))
-
-    return (
-        <section>
-            <h2>Add a New Post</h2>
-            <form>
-                <label htmlFor="postTitle">Post Title:</label>
-                <input
-                    type="text"
-                    id="postTitle"
-                    name="postTitle"
-                    value={title}
-                    onChange={onTitleChanged}
-                />
-                <label htmlFor="postAuthor">Author:</label>
-                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-                    <option value=""></option>
-                    {usersOptions}
-                </select>
-                <label htmlFor="postContent">Content:</label>
-                <textarea
-                    id="postContent"
-                    name="postContent"
-                    value={content}
-                    onChange={onContentChanged}
-                />
-                <button
-                    type="button"
-                    onClick={onSavePostClicked}
-                    disabled={!canSave}
-                >Save Post</button>
-            </form>
-        </section>
-    )
+  return (
+    <section>
+        <h2>Add a New Post</h2>
+        <form>
+            <label htmlFor="postTitle">Post Title:</label>
+            <input
+                type="text"
+                id="postTitle"
+                name="postTitle"
+                value={title}
+                onChange={onTitleChanged} 
+            />
+            <label htmlFor="postContent">Content:</label>
+            <textarea
+                id="postContent"
+                name="postContent"
+                value={content}
+                onChange={onContentChanged} 
+            />
+            <button 
+                type="button"
+                onClick={onSavePostClicked}
+            >Save Post</button>
+        </form>
+    </section>
+  )
 }
+
 export default AddPostForm
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,36 +1,22 @@
-import { useSelector, useDispatch } from "react-redux";
-import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "./postsSlice";
-import { useEffect } from "react";
-import PostsExcerpt from "./PostsExcerpt";
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
 
 const PostsList = () => {
-    const dispatch = useDispatch();
+    const posts = useSelector(selectAllPosts())
 
-    const posts = useSelector(selectAllPosts);
-    const postStatus = useSelector(getPostsStatus);
-    const error = useSelector(getPostsError);
+    const renderedPosts = posts.map(post => (
+        <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0, 100)}</p>
+        </article>
+    ))
 
-    useEffect(() => {
-        if (postStatus === 'idle') {
-            dispatch(fetchPosts())
-        }
-    }, [postStatus, dispatch])
-
-    let content;
-    if (postStatus === 'loading') {
-        content = <p>"Loading..."</p>;
-    } else if (postStatus === 'succeeded') {
-        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
-    } else if (postStatus === 'failed') {
-        content = <p>{error}</p>;
-    }
-
-    return (
-        <section>
-            <h2>Posts</h2>
-            {content}
-        </section>
-    )
+  return (
+    <section>
+        <h2>Posts</h2>
+        {renderedPosts}
+    </section>
+  )
 }
+
 export default PostsList
